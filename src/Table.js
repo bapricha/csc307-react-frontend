c0ac7735c495a7218c6d1b76fd77d1d8e31196f5
bapricha <bapricha@calpoly.edu>
2023-01-13 13:46:25 -0800

    remove button

--- src/Table.js
+++ src/Table.js
@@ -4,7 +4,7 @@ function Table(props) {
     return (
         <table>
             <TableHeader />
-            <TableBody characterData={props.characterData} />
+            <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
         </table>
     );  
 }
@@ -15,6 +15,7 @@ function TableHeader() {
             <tr>
                 <th>Name</th>
                 <th>Job</th>
+                <th>Remove</th>
             </tr>
         </thead>
     );
@@ -25,7 +26,9 @@ function TableBody(props) {
         return (
             <tr key={index}>
                 <td>{row.name}</td>
-                <td>{row.job}</td>
+                <td>{row.job}</td><td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         );
     }
