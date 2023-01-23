c30096cbfd6193602e728425abd857a0459bb6ee
bapricha <bapricha@calpoly.edu>
2023-01-22 18:53:32 -0800

    First communication with backend - fetchAll and useEffect

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,7 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from 'react';
 import Table from "./Table.js"
 import Form from "./Form.js"
+import axios from 'axios';
 
 function MyApp() {
     const [characters, setCharacters] = useState([ ]);  
@@ -16,6 +17,29 @@ function MyApp() {
         setCharacters(updated);
     }
     
+    async function fetchAll(){
+      try {
+         const response = await axios.get('http://localhost:5000/users');
+         return response.data.users_list;     
+      }
+      catch (error){
+         //We're not handling errors. Just logging into the console.
+         console.log(error); 
+         return false;         
+      }
+   }
+
+   useEffect(() => {
+    fetchAll().then( result => {
+       if (result)
+          setCharacters(result);
+     });
+  }, [] );
+
+
+
+
+
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter = {removeOneCharacter} />
